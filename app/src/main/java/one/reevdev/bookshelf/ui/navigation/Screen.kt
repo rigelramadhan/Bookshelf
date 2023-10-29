781eac551ca29301ae5e99b14cc8ead9c577e4b1
Rigel Ramadhan <rigel.vibi@ai.astra.co.id>
2023-10-29 08:59:20 +0700

    Cleanup

--- app/src/main/java/one/reevdev/bookshelf/ui/navigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package one.reevdev.bookshelf.ui.navigation
-
-sealed class Screen(val route: String, val title: String) {
-    class Home(title: String = "Bookshelf") : Screen(route = "home", title = title)
-    class Detail(title: String = "Detail") : Screen(route = "detail/{bookId}", title = title) {
-        fun createRoute(bookId: String) = "detail/$bookId"
-    }
-}
